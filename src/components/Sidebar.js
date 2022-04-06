be302d4599ab152e8e44ae160632778718de7987
thesaam <samuel@wedeydo.com>
2022-04-06 23:45:05 +0100

    Initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <h3>Metacare</h3>
+        <p>adeyinka@metacare.app</p>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
